4f9b40fa0d64d06670bf7487e898cc2a9ef9cdc4
Rikardo301 <ricardo.barcelo.t@gmail.com>
2025-08-09 23:04:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    if (validarNombre(nombre)) {
+        amigos.push(nombre);
+        actualizarLista();
+        document.getElementById("amigo").value = ""; //Limpiar el campo de entrada
+    } else {
+        alert("Inserta un nombre.");
+    }
+}
+
+function validarNombre(nombre) {
+    return nombre.trim() !== "";
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar la lista actual
+    amigos.forEach(function(amigo) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay nombres para sortear.");
+    }
+    else {
+        let indiceAmigo = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAmigo];
+        document.getElementById("resultado").textContent = "Amigo sorteado: " + amigoSorteado;
+    }
+}
